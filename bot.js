bbfa948e6cdb3476cdb024757ba74c1e2c97dce3
Mike Quinn <mike.quinn@scotland.pnn.police.uk>
2017-11-09 16:16:55 +0000

    Updated regex

--- bot.js
+++ bot.js
@@ -13,3 +13,8 @@ const listeners = 'direct_message, direct_mention, mention';
 controller.hears(['hello'], listeners, (bot, message) => {
   bot.reply(message, 'Hey there lil fellar');
 });
+
+controller.hears(myReg, ['message_received'], (bot, message) => {
+  bot.reply(message, `You are fined one credit for a
+    violation of the Verbal Morality Statutes!`);
+});

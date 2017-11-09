06c162e66e50477439eee33d474a5fb2a6a40732
Mike Quinn <mike.quinn@scotland.pnn.police.uk>
2017-11-09 16:31:29 +0000

    Updated regex

--- bot.js
+++ bot.js
@@ -9,12 +9,16 @@ controller.spawn({
   token: process.env.token
 }).startRTM(err => console.log(err));
 
+controller.hears(myReg, ['message_received'], (bot, message) => {
+  bot.reply(message, `You are fined one credit for a
+    violation of the Verbal Morality Statutes!`);
+});
+
+/*
 const listeners = 'direct_message, direct_mention, mention';
 controller.hears(['hello'], listeners, (bot, message) => {
   bot.reply(message, 'Hey there lil fellar');
 });
+*/
 
-controller.hears(['shit'], ['message_received'], (bot, message) => {
-  bot.reply(message, `You are fined one credit for a
-    violation of the Verbal Morality Statutes!`);
-});
+

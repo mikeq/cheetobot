f53037d1cbe755e96599d25b924243b89becc333
Mike Quinn <mike@velochallenge.org>
2017-11-11 09:08:50 +0000

    Mongo integration

--- components/spartan.js
+++ components/spartan.js
@@ -35,18 +35,17 @@ const getUserInfo = (bot, user) => {
 };
 
 exports.sendMessage = async (bot, message) => {
-  console.log('Getting user info from slack');
   let userInfo = await getUserInfo(bot, message.user);
-  console.log('Got user info from slack');
-  console.log('Getting demolition record from mongo');
   let demolitionRecord = await getDemolitionRecord(message.user, userInfo.profile.first_name);
-  console.log('Got demolition record from mongo');
+  let credits = 0;
   if (demolitionRecord.length > 0) {
-    updateCredit(demolitionRecord[0])
+    credits = demolitionRecord[0].credits;
+    updateCredit(demolitionRecord[0]);
   } else {
     createDemolitionRecord(userInfo);
   }
   console.log('Sending message');
   bot.reply(message, `${userInfo.profile.first_name} you are fined one credit for a
-    violation of the Verbal Morality Statutes!`);
+    violation of the Verbal Morality Statutes!
+    You have been fined a total of ${++credits}`);
 }
